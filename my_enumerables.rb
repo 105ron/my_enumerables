13a38da0bb57b1a992cdbe8ae0fe5af98285cb71
rhys <rbrooker105@gmail.com>
2016-09-18 11:47:36 +1000

    Refactor code for edge cases

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,7 +1,7 @@
 module Enumerable
   
   def my_each
-    return self unless block_given?
+    return "#<Enumerator: #{self}:my_each>" unless block_given?
     i = 0
     while i < self.size
       yield(self[i])  
@@ -12,7 +12,7 @@ module Enumerable
 
 
   def my_each_with_index
-  	return self unless block_given?
+  	return "#<Enumerator: #{self}:my_each_with_index>" unless block_given?
     i = 0
     while i < self.size
       yield(self[i], i)  
@@ -23,6 +23,7 @@ module Enumerable
   
 
   def my_select
+    return "#<Enumerator: #{self}:my_select>" unless block_given?
     new_array = []
     my_each { |i| new_array << i if yield(i) }
 		new_array
@@ -30,24 +31,37 @@ module Enumerable
 
 
   def my_all?
-    my_each { |i| return false unless yield(i) }
-		return true
+    if block_given?
+      my_each { |i| return false unless yield(i) }
+    else
+      return true
+    end
+    return true
   end
-
+  #edge cases tested to here
 
   def my_none?
-    my_each { |i| return true unless yield(i) }
-		return false
+    if block_given?
+      my_each { |i| return false if yield(i) }
+		  return true
+    else
+      return false
+    end
   end
 
 
   def my_count
-  	iterator = 0
-  	my_each { iterator +=1 if yield(i) }
-  	iterator
+    if block_given?
+      iterator = 0
+      my_each { iterator +=1 if yield(i) }
+      iterator
+    else
+      return self.size
+    end
   end
 
   def my_map
+    return "#<Enumerator: #{self}:map>" unless block_given?
     new_array = []
     my_each {|i| new_array << yield(i)}
     self.replace new_array
