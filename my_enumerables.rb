8225cbed22945329fd568ec70d6587c262386291
rhys <rbrooker105@gmail.com>
2016-09-18 10:28:07 +1000

    my_map working

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -9,7 +9,7 @@ module Enumerable
     end
     self
   end
-  
+
 
   def my_each_with_index
   	return self unless block_given?
@@ -30,17 +30,29 @@ module Enumerable
 
 
   def my_all?
-  	new_array = []
     my_each { |i| return false unless yield(i) }
 		return true
   end
 
 
   def my_none?
-  	new_array = []
     my_each { |i| return true unless yield(i) }
 		return false
   end
 
-  
+
+  def my_count
+  	iterator = 0
+  	my_each { iterator +=1 if yield(i) }
+  	iterator
+  end
+
+  def my_map
+    new_array = []
+    my_each {|i| new_array << yield(i)}
+    self.replace new_array
+    return self
+  end
+
+
 end
